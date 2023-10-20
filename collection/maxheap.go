fa91598c1a6937f93e987f73bad6c8089540bff6
Felix Hoffmann <19827840+zzzFelix@users.noreply.github.com>
2023-10-20 20:57:08 +0200

    feat: max heap

--- collection/maxheap.go
+++ collection/maxheap.go
@@ -1,45 +1,108 @@
 package collection
 
-import "errors"
+import (
+	"errors"
+)
 
-type heapElement[T comparable] struct {
+type maxHeap[T comparable] struct {
+	items []heapItem[T]
+}
+
+type heapItem[T comparable] struct {
 	object   T
 	priority int
 }
 
-type MaxHeap[T comparable] []heapElement[T]
+type MaxHeap[T comparable] maxHeap[T]
 
-func (h MaxHeap[T]) Insert(element T, priority int) {
-	mHeapElem := heapElement[T]{
+func New[T comparable](items []T, priorities []int) (*MaxHeap[T], error) {
+	size := len(items)
+	if size != len(priorities) {
+		return nil, errors.New("length of items is not equal to length of priorities")
+	}
+	mHeap := MaxHeap[T]{items: []heapItem[T]{}}
+	for i := range items {
+		mHeap.Insert(items[i], priorities[i])
+	}
+	return &mHeap, nil
+}
+
+func (h *MaxHeap[T]) Insert(element T, priority int) {
+	mHeapElem := heapItem[T]{
 		object:   element,
 		priority: priority,
 	}
-	h[0] = mHeapElem
+	h.items = append(h.items, mHeapElem)
+	h.siftUp(h.Size() - 1)
 }
 
-func (h MaxHeap[T]) Peek() T {
-	return h[0].object
+func (h *MaxHeap[T]) Peek() T {
+	return h.items[0].object
 }
 
-func (h MaxHeap[T]) siftUp() {
-
+func (h *MaxHeap[T]) Pop() T {
+	size := len(h.items)
+	elem := h.items[size-1]
+	h.items[0] = h.items[size-1]
+	h.items = h.items[:size-1]
+	h.siftDown(0)
+	return elem.object
 }
 
-func (h MaxHeap[T]) siftDown() {
-
+func (h *MaxHeap[T]) Size() int {
+	return len(h.items)
 }
 
-func New[T comparable](elements []T, priorities []int) (MaxHeap[T], error) {
-	size := len(elements)
-	if size != len(priorities) {
-		return nil, errors.New("length of elements is not equal to length of priorities")
+func (h *MaxHeap[T]) siftUp(i int) {
+	current := h.items[i].priority
+	parentI := (i - 1) / 2
+	if parentI < 0 {
+		return
 	}
-	mHeap := make(MaxHeap[T], size)
-	for i := range elements {
-		mHeap[i] = heapElement[T]{
-			object:   elements[i],
-			priority: priorities[i],
+	parent := h.items[parentI].priority
+	if current > parent {
+		tmp := h.items[parentI]
+		h.items[parentI] = h.items[i]
+		h.items[i] = tmp
+		h.siftUp(parentI)
+	}
+}
+
+func (h *MaxHeap[T]) siftDown(i int) {
+	childAI := (2 * i) + 1
+	childBI := (2 * i) + 2
+	if childAI >= len(h.items) {
+		return
+	}
+	current := h.items[i].priority
+	childA := h.items[childAI].priority
+	if childBI >= len(h.items) {
+		if current < childA {
+			tmp := h.items[childAI]
+			h.items[childAI] = h.items[i]
+			h.items[i] = tmp
+			h.siftDown(childAI)
 		}
+		return
 	}
-	return mHeap, nil
+	childB := h.items[childBI].priority
+	if current < childA {
+		if childA < childB {
+			tmp := h.items[childBI]
+			h.items[childBI] = h.items[i]
+			h.items[i] = tmp
+			h.siftDown(childBI)
+		} else {
+			tmp := h.items[childAI]
+			h.items[childAI] = h.items[i]
+			h.items[i] = tmp
+			h.siftDown(childAI)
+		}
+
+	} else if current < childB {
+		tmp := h.items[childBI]
+		h.items[childBI] = h.items[i]
+		h.items[i] = tmp
+		h.siftDown(childBI)
+	}
 }
--- collection/maxheap_test.go
+++ collection/maxheap_test.go
@@ -1,16 +1,14 @@
 package collection
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestMaxHeap(t *testing.T) {
 	t.Run("should insert element into heap", func(t *testing.T) {
 		// given
 		testS := "Hello world"
-		heap, err := New[string]([]string{testS}, []int{1})
-
-		if err != nil {
-			t.Fatal("Heap creation resulted in error")
-		}
+		heap, _ := New[string]([]string{testS}, []int{1})
 
 		// when
 		heap.Insert(testS, 99)
@@ -19,6 +17,88 @@ func TestMaxHeap(t *testing.T) {
 		actual := heap.Peek()
 		if testS != actual {
 			t.Fatal("Wrong element returned")
+		}
+		if heap.Size() != 2 {
+			t.Fatalf("Wrong heap size. Expected: %d, actual: %d", 2, heap.Size())
+		}
+	})
+
+	t.Run("should create heap with 5 elements and return highest priority", func(t *testing.T) {
+		// given
+
+		// when
+		heap, _ := New[string](
+			[]string{"low prio", "low prio", "low prio", "low prio", "HIGH PRIO", "low prio", "low prio"},
+			[]int{12, 44, 103, 12, 99999, 12, 13},
+		)
+
+		// then
+		actual := heap.Peek()
+		if actual != "HIGH PRIO" {
+			t.Fatalf("Wrong element returned. Actual: %s", actual)
+		}
+		if heap.Size() != 7 {
+			t.Fatalf("Wrong heap size. Expected: %d, actual: %d", 7, heap.Size())
+		}
+	})
+
+	t.Run("should add 5 elements and return highest priority", func(t *testing.T) {
+		// given
+		heap, _ := New[string]([]string{}, []int{})
+
+		// when
+		heap.Insert("low prio", 12)
+		heap.Insert("low prio", 44)
+		heap.Insert("low prio", 103)
+		heap.Insert("low prio", 12)
+		heap.Insert("HIGH PRIO", 999)
+		heap.Insert("low prio", 12)
+		heap.Insert("low prio", 13)
+
+		// then
+		actual := heap.Peek()
+		if actual != "HIGH PRIO" {
+			t.Fatalf("Wrong element returned. Actual: %s", actual)
+		}
+		if heap.Size() != 7 {
+			t.Fatalf("Wrong heap size. Expected: %d, actual: %d", 7, heap.Size())
+		}
+	})
+
+	t.Run("should delete element", func(t *testing.T) {
+		// given
+		testS := "Hello world"
+		heap, _ := New[string]([]string{testS}, []int{1})
+
+		// when
+		heap.Pop()
+
+		// then
+		if heap.Size() != 0 {
+			t.Fatalf("Wrong heap size. Expected: %d, actual: %d", 0, heap.Size())
+		}
+	})
+
+	t.Run("should delete all but one element, last element should be lowest prio", func(t *testing.T) {
+		// given
+		heap, _ := New[string](
+			[]string{"default prio", "default prio", "default prio", "LOWEST PRIO", "default prio", "default prio", "default prio"},
+			[]int{12, 44, 103, 0, 99999, 12, 13},
+		)
+
+		// when
+		heapSize := heap.Size() - 1
+		for i := 0; i < heapSize; i++ {
+			heap.Pop()
+		}
+
+		// then
+		actual := heap.Peek()
+		if actual != "LOWEST PRIO" {
+			t.Fatalf("Wrong element returned. Actual: %s", actual)
+		}
+		if heap.Size() != 1 {
+			t.Fatalf("Wrong heap size. Expected: %d, actual: %d", 1, heap.Size())
 		}
 	})
 }
