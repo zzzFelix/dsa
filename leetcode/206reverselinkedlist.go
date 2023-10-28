6be5b6ab7aee9935fea865f5b67e0d510923bf8d
Felix Hoffmann <19827840+zzzFelix@users.noreply.github.com>
2023-10-28 20:44:01 +0200

    feat: leetcode 206 reverse linked list

---
+++ leetcode/206reverselinkedlist.go
@@ -0,0 +1,25 @@
+package leetcode
+
+import "dsa/core/collection"
+
+/**
+ * https://leetcode.com/problems/reverse-linked-list/
+ *
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Value int
+ *     Next *collection.Node[int]
+ * }
+ */
+func reverseList(head *collection.Node[int]) *collection.Node[int] {
+	return reverseListHelper(head, nil)
+}
+
+func reverseListHelper(head *collection.Node[int], prev *collection.Node[int]) *collection.Node[int] {
+	if head == nil {
+		return prev
+	}
+	tmp := head.Next
+	head.Next = prev
+	return reverseListHelper(tmp, head)
+}
---
+++ leetcode/206reverselinkedlist_test.go
@@ -0,0 +1,69 @@
+package leetcode
+
+import (
+	"dsa/core/collection"
+	"testing"
+)
+
+func TestReverseLinkedList(t *testing.T) {
+	t.Run("reverse 5 element list", func(t *testing.T) {
+		// given
+		ll := collection.LinkedList[int]{}
+		node1 := ll.CreateNode(1)
+		node2 := ll.CreateNode(2)
+		node3 := ll.CreateNode(3)
+		node4 := ll.CreateNode(4)
+		node5 := ll.CreateNode(5)
+		node1.Next = &node2
+		node2.Next = &node3
+		node3.Next = &node4
+		node4.Next = &node5
+
+		// when
+		actual := reverseList(&node1)
+
+		// then
+		expected := []int{5, 4, 3, 2, 1}
+		current := actual
+		for _, val := range expected {
+			if current.Value != val {
+				t.Fatal("values aren't equal")
+			}
+			current = current.Next
+		}
+	})
+
+	t.Run("reverse 2 element list", func(t *testing.T) {
+		// given
+		ll := collection.LinkedList[int]{}
+		node1 := ll.CreateNode(1)
+		node2 := ll.CreateNode(2)
+		node1.Next = &node2
+
+		// when
+		actual := reverseList(&node1)
+
+		// then
+		expected := []int{2, 1}
+		current := actual
+		for _, val := range expected {
+			if current.Value != val {
+				t.Fatal("values aren't equal")
+			}
+			current = current.Next
+		}
+	})
+
+	t.Run("reverse empty list", func(t *testing.T) {
+		// given
+		var expected *collection.Node[int] = nil
+
+		// when
+		actual := reverseList(expected)
+
+		// then
+		if expected != actual {
+			t.Fatal("result list is not empty")
+		}
+	})
+}
