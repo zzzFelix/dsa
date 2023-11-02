b50774c5c6f1e8185919daf7823076cdf8dd476c
Felix Hoffmann <19827840+zzzFelix@users.noreply.github.com>
2023-11-02 19:03:28 +0100

    test: binary search

---
+++ core/search/binarysearch_test.go
@@ -0,0 +1,173 @@
+package search
+
+import (
+	"dsa/core/compare"
+	"testing"
+)
+
+func TestBinarySearch(t *testing.T) {
+	t.Run("should find middle number of 3", func(t *testing.T) {
+		// given
+		bSearch := BinarySearch[int]{
+			sortedItems: []int{1, 2, 3},
+			comparator:  compare.IntComparator{},
+		}
+
+		// when
+		index, error := bSearch.Search(2)
+
+		// then
+		if error != nil {
+			t.Fatalf("Search threw error: %v", error)
+		}
+		if index != 1 {
+			t.Fatalf("Search returned wrong index. Expected: 1, actual: %d", index)
+		}
+	})
+
+	t.Run("should find left item of 2", func(t *testing.T) {
+		// given
+		bSearch := BinarySearch[int]{
+			sortedItems: []int{1, 2},
+			comparator:  compare.IntComparator{},
+		}
+
+		// when
+		index, error := bSearch.Search(1)
+
+		// then
+		if error != nil {
+			t.Fatalf("Search threw error: %v", error)
+		}
+		if index != 0 {
+			t.Fatalf("Search returned wrong index. Expected: 0, actual: %d", index)
+		}
+	})
+
+	t.Run("should find right item of 2", func(t *testing.T) {
+		// given
+		bSearch := BinarySearch[int]{
+			sortedItems: []int{1, 2},
+			comparator:  compare.IntComparator{},
+		}
+
+		// when
+		index, error := bSearch.Search(2)
+
+		// then
+		if error != nil {
+			t.Fatalf("Search threw error: %v", error)
+		}
+		if index != 1 {
+			t.Fatalf("Search returned wrong index. Expected: 1, actual: %d", index)
+		}
+	})
+
+	t.Run("should find single item", func(t *testing.T) {
+		// given
+		bSearch := BinarySearch[int]{
+			sortedItems: []int{1},
+			comparator:  compare.IntComparator{},
+		}
+
+		// when
+		index, error := bSearch.Search(1)
+
+		// then
+		if error != nil {
+			t.Fatalf("Search threw error: %v", error)
+		}
+		if index != 0 {
+			t.Fatalf("Search returned wrong index. Expected: 0, actual: %d", index)
+		}
+	})
+
+	t.Run("should find left-most item of many", func(t *testing.T) {
+		// given
+		bSearch := BinarySearch[int]{
+			sortedItems: []int{1, 2, 3, 4, 5, 6, 99, 102, 4065, 423423, 423424, 423425, 423426},
+			comparator:  compare.IntComparator{},
+		}
+
+		// when
+		index, error := bSearch.Search(1)
+
+		// then
+		if error != nil {
+			t.Fatalf("Search threw error: %v", error)
+		}
+		if index != 0 {
+			t.Fatalf("Search returned wrong index. Expected: 0, actual: %d", index)
+		}
+	})
+
+	t.Run("should find right-most item of many", func(t *testing.T) {
+		// given
+		bSearch := BinarySearch[int]{
+			sortedItems: []int{1, 2, 3, 4, 5, 6, 99, 102, 4065, 423423, 423424, 423425, 423426},
+			comparator:  compare.IntComparator{},
+		}
+
+		// when
+		index, error := bSearch.Search(423426)
+
+		// then
+		if error != nil {
+			t.Fatalf("Search threw error: %v", error)
+		}
+		if index != 12 {
+			t.Fatalf("Search returned wrong index. Expected: 12, actual: %d", index)
+		}
+	})
+
+	t.Run("should find random item of many", func(t *testing.T) {
+		// given
+		bSearch := BinarySearch[int]{
+			sortedItems: []int{1, 2, 3, 4, 5, 6, 99, 102, 4065, 423423, 423424, 423425, 423426},
+			comparator:  compare.IntComparator{},
+		}
+
+		// when
+		index, error := bSearch.Search(5)
+
+		// then
+		if error != nil {
+			t.Fatalf("Search threw error: %v", error)
+		}
+		if index != 4 {
+			t.Fatalf("Search returned wrong index. Expected: 4, actual: %d", index)
+		}
+	})
+
+	t.Run("should return error when item not in list", func(t *testing.T) {
+		// given
+		bSearch := BinarySearch[int]{
+			sortedItems: []int{1, 2, 3, 4, 5, 6, 99, 102, 4065, 423423, 423424, 423425, 423426},
+			comparator:  compare.IntComparator{},
+		}
+
+		// when
+		_, error := bSearch.Search(8)
+
+		// then
+		if error == nil {
+			t.Fatal("Search have thrown error")
+		}
+	})
+
+	t.Run("should return error when list empty", func(t *testing.T) {
+		// given
+		bSearch := BinarySearch[int]{
+			sortedItems: []int{},
+			comparator:  compare.IntComparator{},
+		}
+
+		// when
+		_, error := bSearch.Search(0)
+
+		// then
+		if error == nil {
+			t.Fatal("Search have thrown error")
+		}
+	})
+}
