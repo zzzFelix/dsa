c18b8fa0b54048d8bad3c855b19812aa8eb9e910
Felix Hoffmann <19827840+zzzFelix@users.noreply.github.com>
2023-12-04 22:00:22 +0100

    refactor: rename linked list node

--- collection/linkedlist.go
+++ collection/linkedlist.go
@@ -1,14 +1,14 @@
 package collection
 
-type Node[T any] struct {
+type LinkedListNode[T any] struct {
 	Value T
-	Next  *Node[T]
+	Next  *LinkedListNode[T]
 }
 
 type LinkedList[T any] struct{}
 
-func (l *LinkedList[T]) CreateNode(obj T) Node[T] {
-	node := Node[T]{
+func (l *LinkedList[T]) CreateNode(obj T) LinkedListNode[T] {
+	node := LinkedListNode[T]{
 		Value: obj,
 		Next:  nil,
 	}
