3b818717dbdd0f455b3138571ebd27129f0eec3c
Felix Hoffmann <19827840+zzzFelix@users.noreply.github.com>
2024-08-30 22:33:42 +0200

    test: unionfind with "find if path exists in graph"

--- unionfind/unionfind_test.go
+++ unionfind/unionfind_test.go
@@ -8,6 +8,41 @@ import (
 )
 
 func TestUnionFind(t *testing.T) {
+	t.Run("find if path exists in graph", func(t *testing.T) {
+		// given
+		source := 5
+		destination := 9
+		edges := [][]int{{4, 3}, {1, 4}, {4, 8}, {1, 7}, {6, 4}, {4, 2}, {7, 4}, {4, 0}, {0, 9}, {5, 4}}
+		expected := true
+
+		// when
+		unions := map[int]*unionfind.Member{}
+
+		for _, edge := range edges {
+			aMem, aOk := unions[edge[0]]
+			bMem, bOk := unions[edge[1]]
+			if !aOk {
+				aMem = unionfind.NewMember(edge[0])
+			}
+			if !bOk {
+				bMem = unionfind.NewMember(edge[1])
+			}
+			member := unionfind.Union(aMem, bMem)
+			unions[edge[0]] = member
+			unions[edge[1]] = member
+		}
+
+		// then
+		srcMem, srcOk := unions[source]
+		destMem, destOk := unions[destination]
+		if srcOk && destOk {
+			actual := unionfind.Find(srcMem) == unionfind.Find(destMem)
+			if expected != actual {
+				t.Fatalf("expected: %v, actual: %v", expected, actual)
+			}
+		}
+	})
+
 	t.Run("Most Stones Removed With Same Row or Column", func(t *testing.T) {
 		rows := map[int]*unionfind.Member{}
 		cols := map[int]*unionfind.Member{}
